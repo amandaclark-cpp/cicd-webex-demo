4d3544138d817d7796926bf5520d29aa1bf3435b
amandaclark-cpp <amandaclark@cpp.edu>
2025-11-13 18:51:22 -0800

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Building the sample project..."
-python sample_code.py
-
-echo "Checking for PEP8 compliance..."
-pycodestyle sample_code.py --max-line-length=100
