8bedd32bc4c5be66bb286a494680f455e26a1c7a
amandaclark-cpp <amandaclark@cpp.edu>
2025-11-13 17:27:52 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 echo "Building the sample project..."
-python3 sample_code.py
+python sample_code.py
 
 echo "Checking for PEP8 compliance..."
-pip3 install --quiet pycodestyle
 pycodestyle sample_code.py --max-line-length=100
